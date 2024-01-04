270d8fcba372940b3b6bb206a07399fa7bd913d5
Roman <149516586+Romamaha@users.noreply.github.com>
2024-01-04 20:37:36 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,8 +3,29 @@
 // Для перевірки завдання введіть команду npm jest task2.test.js
 
 
-// function applyStyles() {
- 
-// }
+function applyStyles() {
+    const styledElement = document.getElementById("title")
+    const firstPar = document.getElementById("myDiv").children[0]
+    const secondPar = document.getElementById("myDiv").children[1]
+    const thirdPar = document.getElementById("myDiv").children[2]
+    const fourthPar = document.getElementById("myDiv").children[3]
+    const myList = document.getElementById("myList")
+    const span = document.querySelector('.style_elements span')
 
-// module.exports = applyStyles;
+    styledElement.style.fontSize = "24px";
+    styledElement.style.backgroundColor = "lightgreen";
+
+    firstPar.style.fontWeight = "700";
+    secondPar.style.color = "red";
+    thirdPar.style.textDecoration = "underline";
+    fourthPar.style.fontStyle = "italic";
+
+    myList.style.listStyle = "none";
+    myList.style.display = "flex";
+
+    span.style.display = "none";
+
+
+ }
+
+ module.exports = applyStyles;
